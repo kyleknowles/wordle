6c42b4e69d75856c4311ce7353a8b26d15b5a059
kyleknowles <115665968+kyleknowles@users.noreply.github.com>
2024-05-14 22:19:47 +0000

    Doesn't allow invalid guesses

--- wordle/wordle_helper.py
+++ wordle/wordle_helper.py
@@ -5,19 +5,35 @@ import numpy as np
 df = pd.read_csv('wordle/valid_freq.csv')
 five_letter_array = df['word'].to_numpy()
 
+df_valid_solutions = pd.read_csv('wordle/valid_solutions.csv')
+valid_solutions = df_valid_solutions['word'].to_numpy()
+
+df_valid_guesses = pd.read_csv('wordle/valid_guesses.csv')
+valid_guesses = df_valid_guesses['word'].to_numpy()
 
 def wordle_solver(five_letter_array):
     print("Welcome to Wordle Helper")
     for guess in range(5):
         word = input("Choose a word: ").lower()
-        while (len(word) != 5):
-            print("Word must be of length 5")
+
+        while (len(word) != 5) | ((word not in valid_guesses) & (word not in valid_solutions)):
+            if (word not in valid_guesses):
+                print("Not a valid guess")
+            if (len(word) != 5):
+                print("Word must be of length 5")
             word = input("Choose a word: ").lower()
 
+
         word_data = input("And what information did that give you?: ").upper()
-        while (len(word_data) != 5):
-            print("Information must be of length 5")
+        while (len(word_data) != 5) | any(letter not in "BGY" for letter in word_data):
+            if (len(word_data) != 5):
+                print("Information must be of length 5")
+            if (any(letter not in "BGY" for letter in word_data)):
+                print("Answer should only contain Bs (Black), Ys (Yellow), and Gs (Green)")
+
             word_data = input("And what information did that give you?: ").upper()
+        
+        
 
         # If you win
         if (word_data == "GGGGG"):
@@ -33,11 +49,9 @@ def wordle_solver(five_letter_array):
                     if ((word_data[letter] == "G") | (word_data[letter] == "Y")) & (word[data_letter] == word[letter]):
                         guess_count += 1
 
-    
                 five_letter_array = [five_word for five_word in five_letter_array if word[data_letter] != five_word[data_letter]]
                 five_letter_array = [five_word for five_word in five_letter_array if five_word.count(word[data_letter]) == guess_count]
                
-
             elif (word_data[data_letter] == "G"):
                 five_letter_array = [five_word for five_word in five_letter_array if word[data_letter] == five_word[data_letter]]
                 
@@ -53,11 +67,10 @@ def wordle_solver(five_letter_array):
             
         for common_word in range (word_length):
             print(str(common_word+1) + ". " + five_letter_array[common_word])
+        
+        if (len(five_letter_array) > 20):
+            print("... + " + str(len(five_letter_array)-20) + " more")
         print()
-                        
 
-
-
-   
 wordle_solver(five_letter_array)
 
