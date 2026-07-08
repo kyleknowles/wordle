7e650a8bb2750be58e92c86ce3cad9b57b8ede1f
kyleknowles <115665968+kyleknowles@users.noreply.github.com>
2026-07-08 23:37:33 +0000

    program names the max value properly this time if there are not enough in the step value

--- geo/makeBirdNames.py
+++ geo/makeBirdNames.py
@@ -19,7 +19,7 @@ step_count = 100
 
 
 
-speciesNum = 1
+speciesNum = 1501
 
 
 
@@ -31,7 +31,7 @@ while (speciesNum <= len(gdf)):
     speciesMaxNum = speciesMinNum + step_count
 
     if (len(gdf) <= speciesMaxNum):
-        speciesMaxNum = len(gdf) + 1
+        speciesMaxNum = len(gdf) - 1
 
 
     vernacularNamesList = []
