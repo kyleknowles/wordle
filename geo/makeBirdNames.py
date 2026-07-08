491009f54ae62b61ddc623feb96b794ddb1e5a05
kyleknowles <115665968+kyleknowles@users.noreply.github.com>
2026-07-08 22:52:34 +0000

    can handle missing scientific Name key errors

--- geo/makeBirdNames.py
+++ geo/makeBirdNames.py
@@ -4,6 +4,7 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from pygbif import species
 from pathlib import Path
+import csv
 
 folder = "birds"
 file = "birds.gpkg"
@@ -17,7 +18,7 @@ step_count = 100
 
 
 
-speciesNum = 4600
+speciesNum = 4601
 
 
 
@@ -34,13 +35,18 @@ while (speciesNum <= len(gdf)):
     curr_gdf = gdf.iloc[speciesMinNum:speciesMaxNum]
     species_list = curr_gdf["name"].to_list()
 
-    for scientificName in species_list: 
-        res = species.name_backbone(scientificName=scientificName) 
-        key = res["usage"]["key"] 
-        details = species.name_usage(key=key) 
-        vernacularName = details.get("vernacularName") 
-        print(str(speciesNum) + ": " + str(scientificName) + ": " + str(vernacularName)) 
-        vernacularNamesList.append(vernacularName) 
+    for scientificName in species_list:
+        res = species.name_backbone(scientificName=scientificName)
+        if "usage" in res: 
+            key = res["usage"]["key"] 
+            details = species.name_usage(key=key) 
+            vernacularName = details.get("vernacularName") 
+            print(str(speciesNum) + ": " + str(scientificName) + ": " + str(vernacularName)) 
+            vernacularNamesList.append(vernacularName)
+        else:
+            vernacularNamesList.append("")
+            with open ("geo/errorvalues.csv", "a") as f:
+                f.write(scientificName + '\n') 
         speciesNum = speciesNum + 1
 
     curr_gdf["Vernacular Name"] = vernacularNamesList
