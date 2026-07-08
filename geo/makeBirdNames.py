468680b105188534b8b5c57816648244e09d42bd
kyleknowles <115665968+kyleknowles@users.noreply.github.com>
2026-07-08 21:08:08 +0000

    can create multiple divided files of birds.gpkg with common names added

--- geo/makeBirdNames.py
+++ geo/makeBirdNames.py
@@ -3,25 +3,45 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from pygbif import species
+from pathlib import Path
 
 
-gdf = gpd.read_file("birds.gpkg")
+file = "birds.gpkg"
+filename = Path(file)
+
+gdf = gpd.read_file(file)
+
+
+step_count = 10
 
 
 
-species_list = gdf["name"].to_list()
-print(species_list)
 
-
-vernacularNamesList = []
 speciesNum = 1
 
 
-for scientificName in species_list: 
-    res = species.name_backbone(scientificName=scientificName) 
-    key = res["usage"]["key"] 
-    details = species.name_usage(key=key) 
-    vernacularName = details.get("vernacularName") 
-    print(str(speciesNum) + ": " + str(scientificName) + ": " + str(vernacularName)) 
-    vernacularNamesList.append(vernacularName) 
-    speciesNum = speciesNum + 1
+
+
+
+while (speciesNum <= len(gdf)):
+    speciesMinNum = speciesNum - 1
+    speciesMaxNum = speciesMinNum + step_count
+
+
+    vernacularNamesList = []
+
+    curr_gdf = gdf.iloc[speciesMinNum:speciesMaxNum]
+    species_list = curr_gdf["name"].to_list()
+
+    for scientificName in species_list: 
+        res = species.name_backbone(scientificName=scientificName) 
+        key = res["usage"]["key"] 
+        details = species.name_usage(key=key) 
+        vernacularName = details.get("vernacularName") 
+        print(str(speciesNum) + ": " + str(scientificName) + ": " + str(vernacularName)) 
+        vernacularNamesList.append(vernacularName) 
+        speciesNum = speciesNum + 1
+
+    curr_gdf["Vernacular Name"] = vernacularNamesList
+
+    curr_gdf.to_csv('geo/birds/'+str(speciesMinNum+1)+"-"+str(speciesMaxNum))
