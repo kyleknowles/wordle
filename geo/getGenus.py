d69e7d0e0f06fb2b027ed63693472c36d4ce8824
kyleknowles <115665968+kyleknowles@users.noreply.github.com>
2026-07-09 21:51:33 +0000

    can get uniqiue genuses of animal order

---
+++ geo/getGenus.py
@@ -0,0 +1,31 @@
+
+import pandas as pd
+from pathlib import Path
+
+
+foldername = "mammals"
+
+animal_list = []
+
+path = Path("animals/" + foldername)
+
+
+for file in path.iterdir():
+    #print(file)
+
+    df = pd.read_csv(file)
+    curr_list = df["Vernacular Name"].to_list()
+
+    #print(bird_list)
+    #print()
+
+    # gets last word of every 
+    lastname = [str(animal).split(" ")[-1] for animal in curr_list]
+    #print(lastname)
+    #print()
+    animal_list = animal_list + lastname
+
+
+unique_last_names = list(set(animal_list))
+unique_last_names.sort()
+print(unique_last_names)
