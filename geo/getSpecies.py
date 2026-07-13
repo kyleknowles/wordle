feffe25369a6e205c47ea84edd1bb7ffe15fbccf
kyleknowles <115665968+kyleknowles@users.noreply.github.com>
2026-07-13 21:30:46 +0000

    gets species names mostly

--- geo/getSpecies.py
+++ geo/getSpecies.py
@@ -4,33 +4,72 @@ import pandas as pd
 from pathlib import Path
 import re
 
-foldername = "fishes"
+foldername = "mammals"
 
 
 path = Path("animals/" + foldername)
 
-animal_list_c = []
-animal_list_ae = []
-animal_list_a_e = []
+
+ani_df = pd.read_csv("ani.csv")
+ani_list = ani_df["Animal"].to_list()
 
 
-curr_genus = "Tuna"
-for file in path.iterdir():
 
-    df = pd.read_csv(file)
-    curr_list = df["Vernacular Name"].to_list()
+for animal in ani_list:
 
-    species_list_c = [animal.capitalize() for animal in curr_list if curr_genus.upper() in str(animal).upper()] # genus in name
-    species_list_ae = [animal.capitalize() for animal in curr_list if str(animal).split(" ")[-1].capitalize() == curr_genus] # genus at end of name
-    species_list_a_e = [animal.capitalize() for animal in curr_list if re.split(r"[ -]",str(animal))[-1].capitalize() == curr_genus] # genus at end of name not including "-"
+    animal_list_c = []
+    animal_list_ae = []
+    animal_list_a_e = []
+
+    curr_genus = animal
+
+    for file in path.iterdir():
+
+        df = pd.read_csv(file)
+        curr_list = df["Vernacular Name"].to_list()
+
+        #species_list_c = [animal.capitalize() for animal in curr_list if curr_genus.upper() in str(animal).upper()] # genus in name
+        species_list_ae = [animal.capitalize() for animal in curr_list if str(animal).split(" ")[-1].capitalize() == curr_genus] # genus at end of name
+        #species_list_a_e = [animal.capitalize() for animal in curr_list if re.split(r"[ -]",str(animal))[-1].capitalize() == curr_genus] # genus at end of name not including "-"
+        
+
+        #animal_list_c = animal_list_c + species_list_c
+        animal_list_ae = animal_list_ae + species_list_ae
+        #animal_list_a_e = animal_list_a_e + species_list_a_e
+
+
+
+    
+    
+    print(animal)
+    #print(animal_list_c)
+    #print()
+    
+    print(animal_list_ae)
+    print()
+    #print(animal_list_a_e)
+    #print()
+    #print()
     
 
-    animal_list_c = animal_list_c + species_list_c
-    animal_list_ae = animal_list_ae + species_list_ae
-    animal_list_a_e = animal_list_a_e + species_list_a_e
+    '''
+    print(animal)
+    print("C: " + str(len(animal_list_c)))
+    print("AE: " + str(len(animal_list_ae)))
+    print("A-E: " + str(len(animal_list_a_e)))
+    print()
+    '''
 
-print(animal_list_c)
-print()
-print(animal_list_ae)
-print()
-print(animal_list_a_e)
+    '''
+    # find differences between a-e and ae
+    if (len(animal_list_a_e) != len(animal_list_ae)):
+        print(animal)
+        print(list(set(animal_list_a_e) - set(animal_list_ae)))
+        print()
+
+    # differences between contains and at end
+    if (len(animal_list_c) != len(animal_list_ae)):
+        print(animal)
+        print(list(set(animal_list_c) - set(animal_list_ae)))
+        print()
+    '''
