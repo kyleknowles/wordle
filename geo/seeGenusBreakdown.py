8e8ad155a1a0b24068b9802507fb44f3a1d02815
kyleknowles <115665968+kyleknowles@users.noreply.github.com>
2026-07-17 21:22:43 +0000

    now keeps Genus name when merging Genuses

--- geo/seeGenusBreakdown.py
+++ geo/seeGenusBreakdown.py
@@ -28,17 +28,21 @@ for order in orders:
         df["Order"] = filename[:-1].capitalize()
     
 
-    df = df.sort_values(by=["Genus", "First Scientific Name"])
+    #df = df.sort_values(by=["Genus", "First Scientific Name"])
+
+    df = df[["Genus", "geometry", "Order"]]
 
     df["geometry"] = df["geometry"].apply(wkt.loads)
     gdf = gpd.GeoDataFrame(df, geometry="geometry", crs="EPSG:4326")
 
-    gdf = gdf.dissolve(by="Genus")
+
+    gdf = gdf.dissolve(by="Genus", aggfunc="first").reset_index()
 
     total_gdf = gpd.GeoDataFrame(
         pd.concat([total_gdf, gdf], ignore_index=True),
         crs=gdf.crs
     )
+
 
 print(total_gdf)
 total_gdf.to_file("master_animal.gpkg", driver="GPKG")
