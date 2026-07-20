677160adb4652107f4884d4a9c993cb56c183fed
kyleknowles <115665968+kyleknowles@users.noreply.github.com>
2026-07-20 18:02:37 +0000

    can create range images for animals

--- geo/geoexplore.py
+++ geo/geoexplore.py
@@ -5,27 +5,45 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import contextily as ctx
 
-gdf = gpd.read_file("birds.gpkg")
+gdf = gpd.read_file("genus_data.gpkg")
 
 # IMPORTANT: convert to Web Mercator (required for basemaps)
 gdf_web = gdf.to_crs(epsg=3857)
 
-fig, ax = plt.subplots(figsize=(10, 10))
+print(gdf_web)
+fig, ax = plt.subplots(figsize=(20, 20))
 
-# plot your bird range
-gdf_web.iloc[[0]].plot(
+# plot your animal range
+
+animal_num = 100
+
+print(gdf.loc[animal_num, "Genus"])
+
+gdf_web.iloc[[animal_num]].plot(
     ax=ax,
     edgecolor="red",
     facecolor="red",
     linewidth=2
 )
+# Force world extent (Web Mercator limits)
+ax.set_xlim(-20037508, 20037508)
+ax.set_ylim(-17037508, 20037508)
 
 # add world basemap
-ctx.add_basemap(ax, source=ctx.providers.OpenStreetMap.Mapnik)
+ctx.add_basemap(
+    ax,
+    source=ctx.providers.OpenStreetMap.Mapnik,
+    zoom=1
+)
 
 ax.set_axis_off()
 
-plt.savefig("world_overlay.png", dpi=300, bbox_inches="tight", facecolor="white")
-plt.show()
+plt.savefig(
+    "world_overlay.png",
+    dpi=300,
+    bbox_inches="tight",
+    facecolor="white"
+)
+plt.close(fig)
 
 
